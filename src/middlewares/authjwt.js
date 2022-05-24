6cb98b0e3e15c88d38172d294d9bcb7f414c5348
Jose Luis Yañez <yanezluis264@gmail.com>
2022-05-24 00:13:40 -0500

    Se agrego verificacion empleado

--- src/middlewares/authjwt.js
+++ src/middlewares/authjwt.js
@@ -17,3 +17,20 @@ export const verifyToken=async(req,res,next)=>{
     }
     
 }
+
+export const verifyTokenEmpleado=async(req,res,next)=>{
+    try {
+        const token=req.headers["x-access-token"];
+        console.log(token);
+        if(!token) return res.status(403).json({message:"No token provider"});
+        const decode=jwt.verify(token,config.superscret);
+        console.log(decode);
+        const cliente=await Cliente.findOne({_id:decode._id,correo:decode.correo},{_id:1});
+        console.log(cliente);
+        if(!cliente) return res.status(403).json({message:"No token provider"});
+        next();  
+    } catch (error) {
+        return res.status(403).json({message:"No authorization"});
+    }
+    
+}
